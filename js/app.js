964529e096dde4b5bbff3da321db4221e2dd724b
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-02-06 18:50:08 -0600

    finished constructors, added local and hourly totals for a grand total.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@
 console.log('Hello World');
 
 
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Grand Total'];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 const allStores = [];
+let footerTotals = [];
+let grandTotal = 0;
 
 function Store(location, minCustHour, maxCustHour, avgSoldCust) {
   this.location = location;
@@ -21,16 +23,16 @@ function Store(location, minCustHour, maxCustHour, avgSoldCust) {
 
 Store.prototype.ranCustHour = function () {
   return Math.floor(Math.random() * (this.maxCustHour - this.minCustHour) + 1) + this.minCustHour;
-}
+};
 
 Store.prototype.calcLocTotal = function () {
-  for (let i = 0; i < hours.length - 1; i++) {
+  for (let i = 0; i < hours.length; i++) {
     let customer = this.ranCustHour();
     let sales = Math.ceil(customer * this.avgSoldCust);
     this.hourlyTotal.push(sales);
     this.dailyLocationTotal += sales;
   }
-}
+};
 
 Store.prototype.renderStores = function () {
   let body = document.getElementById('storeLocations');
@@ -47,7 +49,7 @@ Store.prototype.renderStores = function () {
   th.textContent = this.dailyLocationTotal;
   tr.appendChild(th);
   body.appendChild(tr);
-}
+};
 
 function renderHeader() {
   let head = document.getElementById('cookieHead');
@@ -58,14 +60,52 @@ function renderHeader() {
     td = document.createElement('td');
     td.textContent = hours[i];
     head.appendChild(td);
-  };
+  }
+  td = document.createElement('td');
+  td.textContent = 'Daily Location Totals';
+  head.appendChild(td);
 }
 
+function renderFooter() {
+  calcFooterTotals();
+  let tfoot = document.getElementById('cookieFoot');
+  let tr = document.createElement('tr');
+  let td = document.createElement('td');
+  td.textContent = 'Total';
+  tr.appendChild(td);
 
-renderHeader();
+  for (let i = 0; i < hours.length; i++) {
+    let td = document.createElement('td');
+    td.textContent = footerTotals[i];
+    tr.appendChild(td);
+  }
+  td = document.createElement('td');
+  td.textContent = grandTotal;
+  tr.appendChild(td);
+
+  tfoot.appendChild(tr);
+}
+
+function calcFooterTotals() {
+  footerTotals = [];
+  grandTotal = 0;
+  for (let i = 0; i < hours.length; i++) {
+    let hourTotal = 0;
+    for (let j = 0; j < allStores.length; j++) {
+      hourTotal += allStores[j].hourlyTotal[i];
+
+    }
+    footerTotals.push(hourTotal);
+    grandTotal += hourTotal;
+    console.log(grandTotal);
+  }
+}
+
 new Store('Seattle', 23, 65, 6.3);
 new Store('Paris', 20, 38, 2.3);
 new Store('Toyko', 3, 24, 1.2);
 new Store('Lima', 2, 16, 4.6);
 new Store('Dubai', 11, 38, 3.7);
+renderHeader();
+renderFooter();
 
